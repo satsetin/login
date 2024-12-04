2ccbb9149c41c7e57a98261a5b7d19a3fc049651
sipatir08 <fathir080604@gmail.com>
2024-12-04 13:11:52 +0700

    pushh

--- qr.js
+++ qr.js
@@ -4,6 +4,6 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1NjI0NDgyNTIxP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
-wauthparam.redirect ="http://127.0.0.1:5502/product.html"
+wauthparam.redirect ="https://satsetin.github.io"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam)
