0bca9aa37150da66b92249cfb6318d923761ab80
sipatir08 <fathir080604@gmail.com>
2024-12-06 16:10:38 +0700

    updatee

--- qr.js
+++ qr.js
@@ -4,6 +4,6 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1NjI0NDgyNTIxP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
-wauthparam.redirect ="https://satsetin.github.io"
+wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam)
